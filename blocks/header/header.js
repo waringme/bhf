07c2438c08a98cd81da06bb3b7ee46baa242f1e8
Tim Waring <waring@adobe.com>
2025-10-02 11:12:59 +0100

    search icon

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -228,7 +228,7 @@ function createSearchBox() {
     const searchInputContainer = div({ class: 'search-input-container' });
     const searchInputBox = document.createElement('input');
     const searchIcon = img({ class: 'search-icon' });
-    searchIcon.src = `${window.hlx.codeBasePath}/icons/search-light.svg`;
+    searchIcon.src = `${window.hlx.codeBasePath}/icons/search.svg`;
     searchIcon.alt = 'search';
     searchIcon.addEventListener('click', () => {
       if (searchInputBox.value) {
@@ -241,7 +241,7 @@ function createSearchBox() {
       type: 'search',
       id: 'search-input',
       name: 'myInput',
-      placeholder: 'Search WKND',
+      placeholder: 'Search BHF',
       value: '',
       autocomplete: 'off',
     });
