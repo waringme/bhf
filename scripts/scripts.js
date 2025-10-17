ae7154d39e2f0cf68b746698d334ed6f41c6e61a
Vrushali Pistulkar <vpistulkar+adobe@adobe.com>
2025-10-17 21:14:06 +0530

    performance fixes
    performance fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -407,7 +407,7 @@ function adjustAutoImages(main) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
+  window.setTimeout(() => import('./delayed.js'), 1000); // Reduced from 3000ms to 1000ms for faster Target initialization
   // load anything that can be postponed to the latest here
 }
 
